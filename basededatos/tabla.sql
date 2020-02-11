67cd34e920a86ff61d4f05de135fd1437e4bace6
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2020-02-11 17:03:52 +0100

    creacion de la tabla, mas el controlador game

---
+++ basededatos/tabla.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Objetos] (
+    [IdJugador]  INT NOT NULL,
+    [barrel]     INT NULL,
+    [soccerBall] INT NULL,
+    [pizza]      INT NULL,
+    [cookie]     INT NULL,
+    [skull]      INT NULL,
+    PRIMARY KEY CLUSTERED ([IdJugador] ASC)
+);
